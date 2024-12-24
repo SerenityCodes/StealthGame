b2ce7898a5bf682bf49160fd36dd6485291f2453
SerenityCodes <72942847+SerenityCodes@users.noreply.github.com>
2024-12-24 08:10:00 -0700

    Provided base for loading a object file from scratch

---
+++ Engine/Source/Engine/Loaders/ModelFromObjFile.cpp
@@ -0,0 +1,14 @@
+﻿#include "ModelFromObjFile.h"
+
+#include <fstream>
+
+engine::Model engine::load_model_from_obj_file(Arena& temp_arena, Arena& model_arena, const char* file_name) {
+    std::ifstream file(file_name);
+    Model model;
+    model.vertices = DynArray<vulkan::VulkanModel::Vertex>{model_arena};
+    model.indices = DynArray<uint32_t>{model_arena};
+    if (!file.is_open()) {
+        return model;
+    }
+    
+}
---
+++ Engine/Source/Engine/Loaders/ModelFromObjFile.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+#include "Engine/Vulkan/VulkanModel.h"
+
+
+namespace engine {
+
+struct Model {
+    Model();
+    DynArray<vulkan::VulkanModel::Vertex> vertices;
+    DynArray<uint32_t> indices;
+};
+
+Model load_model_from_obj_file(Arena& temp_arena, Arena& model_arena, const char* file_name);
+
+}
